fad53978d2b391b1b984007215194beaecb94c03
Arya Antaputra <petewarrior@gmail.com>
2017-01-19 10:06:19 +0700

    add video game context type

---
+++ src/ContextTypes/VideoGame.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace JsonLd\ContextTypes;
+
+class VideoGame extends AbstractContext
+{
+    /**
+     * Property structure
+     *
+     * @var array
+     */
+    protected $structure = [
+        'name' => null,
+        'url' => null,
+        'aggregateRating' => AggregateRating::class,
+        'author' => Person::class,
+        'creator' => Person::class,
+        'dateCreated' => null,
+        'dateModified' => null,
+        'datePublished' => null,
+        'headline' => null,
+        'image' => ImageObject::class,
+        'inLanguage' => null,
+    	'operatingSystem' => null,
+        'publisher' => Organization::class,
+        'review' => Review::class,
+        'text' => null,
+        'thumbnailUrl' => null,
+        'video' => VideoObject::class,
+    ];
+
+    /**
+     * Set the article body attribute.
+     *
+     * @param  string $txt
+     * @return array
+     */
+    protected function setTextAttribute($txt)
+    {
+        return $this->truncate($txt, 260);
+    }
+}
